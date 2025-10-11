2fc91e2ada5d69ef5dbb98650b5b31657a3d7321
AntiiRain <antiirain1412@gmail.com>
2025-10-11 13:01:14 -0400

    hw1 initial update

---
+++ hw1/graph_operations.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lennard on 25-10-11.
+//
+
+#include "graph_operations.h"
---
+++ hw1/graph_operations.h
@@ -0,0 +1,12 @@
+//
+// Created by lennard on 25-10-11.
+//
+
+#ifndef HW1__GRAPH_OPERATIONS_H_
+#define HW1__GRAPH_OPERATIONS_H_
+
+class graph_operations {
+
+};
+
+#endif //HW1__GRAPH_OPERATIONS_H_
